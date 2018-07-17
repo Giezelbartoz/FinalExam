a25b67f18a2334fc1b831bbba0597a5022567924
Benny0013 <bennyputra0013@gmail.com>
2018-07-18 06:18:36 +0700

    New
    - Penambahan Laut
    - Kapalselamnya tak bikin di udara dulu
    - Posisi kapal sudah lurus
    - Objek OK
    - Bola itu buat apa?
    - kurang nambah sirip
    - kuurang nambah pergerakan / movement

--- src/org/yourorghere/GLRenderer.java
+++ src/org/yourorghere/GLRenderer.java
@@ -1,75 +1,117 @@
 package org.yourorghere;
+
+import com.sun.opengl.util.texture.Texture;
+import com.sun.opengl.util.texture.TextureIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
+import static org.yourorghere.Objek.texture;
 
 public class GLRenderer implements GLEventListener {
 
-Objek Tabung=null;
-float R_Z=0.0f, R_X=0.0f, R_Y=0.0f;
-float T_Z= -16.0f, T_X=0.0f, T_Y=0.0f;
-GLAutoDrawable this_drawable=null;
-public void init(GLAutoDrawable drawable) {
-this_drawable=drawable;
-GL gl = drawable.getGL();
-gl.setSwapInterval(1);
-float ambient[] = {1.0f,1.0f,1.0f,1.0f };
-float diffuse[] = {1.0f,1.0f,1.0f,1.0f };
-float position[] = {1.0f,1.0f,1.0f,0.0f };
-gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambient,0);
-gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, diffuse,0);
-gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, position,0);
-gl.glEnable(GL.GL_LIGHT0);
-gl.glEnable(GL.GL_LIGHTING);
-gl.glEnable(GL.GL_DEPTH_TEST);
-gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-gl.glShadeModel(GL.GL_SMOOTH);
-Tabung=new Objek();
-}
-public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-GL gl = drawable.getGL();
-GLU glu = new GLU();
-if (height <= 0) { height = 1;}
-final float h = (float) width / (float) height;
-gl.glViewport(30, 6, width, height);
-gl.glMatrixMode(GL.GL_PROJECTION);
-gl.glLoadIdentity();
-glu.gluPerspective(45.0f, h, 1.0, 20.0);
-gl.glMatrixMode(GL.GL_MODELVIEW);
-gl.glLoadIdentity();
-}
-public void display(GLAutoDrawable drawable) {
-GL gl = drawable.getGL();
-gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-gl.glLoadIdentity();
-//Perintah untuk Translasi sesuai nilai X,Y dan Z
-gl.glTranslatef(-T_X, T_Y, T_Z);
-// Membuat posisi objek tabung berdiri tegak
-gl.glRotatef(-90.0f,1.0f,0.0f,0.0f);
-gl.glRotatef(180.0f,0.0f,0.0f,1.0f);
-//Perintah untuk Rotasi sesuai nilai X,Y dan Z
-gl.glRotatef(R_Z,0.0f,0.0f,1.0f);
-gl.glRotatef(R_X,1.0f,0.0f,0.0f);
-gl.glRotatef(R_Y,0.0f,1.0f,0.0f);
-Objek.kapal(gl);
-gl.glFlush();
-}
-public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
-}
-public void setBasicRotation(float z,float x)
-{
-R_Z=z;
-R_X=x;
-}
-public void R_Tambah_Z(boolean on){ R_Z=R_Z+30.0f;}
-public void R_Tambah_X(boolean on){ R_X=R_X+30.0f;}
-public void R_Tambah_Y(boolean on){ R_Y=R_Y+30.0f;}
-public void T_Tambah_Z(boolean on){ T_Z=T_Z+ 1.0f;}
-public void T_Tambah_X(boolean on){ T_X=T_X+ 1.0f;}
-public void T_Tambah_Y(boolean on){ T_Y=T_Y+ 1.0f;}
+    class vector {
+
+        float x;
+        float y;
+        float z;
+
+        public vector(float startX, float startY, float startZ) {
+            x = startX;
+            y = startY;
+            z = startZ;
+        }
+    }
+
+    vector vertikal = new vector(0f, 1f, 0f);//deklarasi awal vetor untuk gerakan naik & turun
+
+    float Cx = 0, Cy = 2.5f, Cz = 0;
+    float Lx = 0, Ly = 2.5f, Lz = -20f;
+
+    boolean ori = true, silinder = false, silinder1 = false, silinder2 = false, silinder3 = false, silinder4 = false, silinder5 = false, silinder6 = false, silinder7 = false, silinder8 = false, silinder9 = false, silinder0 = false, kamera = false;
+
+    public void init(GLAutoDrawable drawable) {
+
+        GL gl = drawable.getGL();
+        System.err.println("INIT GL IS: " + gl.getClass().getName());
+
+        // Enable VSync
+        gl.setSwapInterval(1);
+        float ambient[] = {1.0f, 1.0f, 1.0f, 1.0f};
+        float diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+        float position[] = {1.0f, 1.0f, 1.0f, 0.0f};
+
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambient, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, diffuse, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, position, 0);
+
+        gl.glEnable(GL.GL_LIGHTING);
+        gl.glEnable(GL.GL_LIGHT0);
+        gl.glEnable(GL.GL_DEPTH_TEST);
+
+        gl.glClearColor(1f, 1f, 1.0f, 1.0f);
+        gl.glShadeModel(GL.GL_SMOOTH);
+
+    }
+
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL gl = drawable.getGL();
+        GLU glu = new GLU();
+
+        if (height <= 0) { // avoid a divide by zero error!
+
+            height = 1;
+        }
+        final float h = (float) width / (float) height;
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL.GL_PROJECTION);
+        gl.glLoadIdentity();
+        glu.gluPerspective(45.0f, h, 1.0, 20.0);
+        gl.glMatrixMode(GL.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+    private static float ox = 0, oy = 5, oz = -15;
+
+    public void display(GLAutoDrawable drawable) {
+
+        GL gl = drawable.getGL();
+        GLU glu = new GLU();
+
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        gl.glLoadIdentity();
+
+        glu.gluLookAt(Cx, Cy + 2, Cz,
+                Lx, Ly, Lz,
+                vertikal.x, vertikal.y, vertikal.z);
+
+        gl.glPushMatrix();
+        //warna
+        float amb1[] = {0.05375f, 0.05f, 0.06625f, 1.0f};
+        float diff1[] = {0.18275f, 0.17f, 0.22525f, 1.0f};
+        float spec1[] = {0.332741f, 0.328634f, 0.346435f, 1.0f};
+        float shine = 34.8f;
+        gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL.GL_AMBIENT, amb1, 0);
+        gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL.GL_DIFFUSE, diff1, 0);
+        gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL.GL_SPECULAR, spec1, 0);
+        gl.glMaterialf(GL.GL_FRONT_AND_BACK, GL.GL_SHININESS, shine);
+        gl.glTranslatef(ox, oy - 2, oz + 2);
+
+        Objek.kapal(gl);
+        gl.glPopMatrix();
+        gl.glPushMatrix();
+        Objek.BigBox(gl);
+
+        gl.glFlush();
+    }
+
+    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+    }
 
     void Key_Pressed(int keyCode) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
 }
